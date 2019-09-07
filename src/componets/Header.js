3e00e6381cc72456a460d1c0a00ec30487e8fb06
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-07 18:38:09 -0400

    Inicio del proyecto y creacion del header y estilos en el index.css

---
+++ src/componets/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = props =>{
+  return(
+    <header>
+      <h1>
+        Cotizador de seguro de auto
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
